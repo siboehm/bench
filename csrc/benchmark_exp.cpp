30064037de643a5724c6e48f4792685c87e2624d
Simon Boehm <simonboehm@mailbox.org>
2022-12-07 16:22:35 +0000

    Work around missing std::expf in libstdc++

--- csrc/benchmark_exp.cpp
+++ csrc/benchmark_exp.cpp
@@ -3,11 +3,13 @@
 
 #include "benchmark/benchmark.h"
 
-// Benchmark the performance of std::expf applied to float32 values.
+float_t expfp32(float_t x) {return std::exp(x);}
+
+// Benchmark the performance of std::exp applied to float32 values.
 static void BM_Expf(benchmark::State &state) {
   // Benchmark the function.
   for (auto _ : state) {
-    benchmark::DoNotOptimize(std::expf(state.range(0)));
+    benchmark::DoNotOptimize(expfp32(state.range(0)));
   }
 }
 BENCHMARK(BM_Expf)->Range(-20, 20);
