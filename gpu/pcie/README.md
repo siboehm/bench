d5b9ab21d2aff3c9a3f12fffb092ec8f83b400ce
Simon Boehm <mail@siboehm.com>
2023-02-22 18:36:45 +0000

    Finish PCIe

---
+++ gpu/pcie/README.md
@@ -0,0 +1,4 @@
+# PCIe
+
+A100 40GB SMX, 16x PCIe 4.0:
+![](pcie.png)
--- gpu/pcie/bench_pcie.cu
+++ gpu/pcie/bench_pcie.cu
@@ -23,8 +23,8 @@ std::string bytes_to_human_readable(size_t size) {
   static const char *sizes[] = {"B", "KB", "MB", "GB", "TB"};
   int order = 0;
   double size_d = (double)size;
-  while (size_d >= 1024 && order < 4) {
-    size_d /= 1024;
+  while (size_d >= 1000 && order < 4) {
+    size_d /= 1000;
     ++order;
   }
 
@@ -35,18 +35,24 @@ std::string bytes_to_human_readable(size_t size) {
   return ss.str();
 }
 
-void runPcieNormal(int size, std::vector<std::chrono::nanoseconds> &time_data,
-                   cudaStream_t stream_in, cudaStream_t stream_out) {
+void runPcieDefault(int size, std::vector<std::chrono::nanoseconds> &time_data,
+                    cudaStream_t stream_in, cudaStream_t stream_out) {
   int sizeBytes = size * sizeof(float);
 
-  float *out_host = (float *)malloc(sizeBytes);
-  float *out_dev;
+  float *in_host, *out_host;
+  in_host = (float *)malloc(sizeBytes);
+  out_host = (float *)malloc(sizeBytes);
+  float *in_dev, *out_dev;
+  CUDA_CHECK(cudaMalloc((void **)&in_dev, sizeBytes));
   CUDA_CHECK(cudaMalloc((void **)&out_dev, sizeBytes));
 
   auto ts = std::chrono::system_clock::now();
 
-  CUDA_CHECK(cudaMemcpyAsync((void **)out_host, out_dev, sizeBytes,
-                             cudaMemcpyDeviceToHost, stream_out));
+  CUDA_CHECK(cudaMemcpyAsync((void **)in_host, out_dev, sizeBytes,
+                             cudaMemcpyDeviceToHost, stream_in));
+  CUDA_CHECK(cudaMemcpyAsync((void **)in_dev, out_host, sizeBytes,
+                             cudaMemcpyHostToDevice, stream_out));
+  CUDA_CHECK(cudaStreamSynchronize(stream_in));
   CUDA_CHECK(cudaStreamSynchronize(stream_out));
 
   time_data.push_back(std::chrono::system_clock::now() - ts);
@@ -64,22 +70,28 @@ void runPciePinned(int size, std::vector<std::chrono::nanoseconds> &time_data,
                    cudaStream_t stream_in, cudaStream_t stream_out) {
   long sizeBytes = size * sizeof(float);
 
-  float *out_host;
-  CUDA_CHECK(cudaMallocHost((void **)&out_host, sizeBytes));
-  float *out_dev;
+  float *in_host, *out_host;
+  CUDA_CHECK(
+      cudaHostAlloc((void **)&out_host, sizeBytes, cudaHostAllocDefault));
+  CUDA_CHECK(cudaHostAlloc((void **)&in_host, sizeBytes, cudaHostAllocDefault));
+  float *in_dev, *out_dev;
+  CUDA_CHECK(cudaMalloc((void **)&in_dev, sizeBytes));
   CUDA_CHECK(cudaMalloc((void **)&out_dev, sizeBytes));
 
   auto ts = std::chrono::system_clock::now();
 
-  CUDA_CHECK(cudaMemcpyAsync((void **)out_host, out_dev, sizeBytes,
-                             cudaMemcpyDeviceToHost, stream_out));
+  CUDA_CHECK(cudaMemcpyAsync((void **)in_host, out_dev, sizeBytes,
+                             cudaMemcpyDeviceToHost, stream_in));
+  CUDA_CHECK(cudaMemcpyAsync((void **)in_dev, out_host, sizeBytes,
+                             cudaMemcpyHostToDevice, stream_out));
+  CUDA_CHECK(cudaStreamSynchronize(stream_in));
   CUDA_CHECK(cudaStreamSynchronize(stream_out));
 
   time_data.push_back(std::chrono::system_clock::now() - ts);
   std::cout << "Size: " << bytes_to_human_readable(sizeBytes)
             << " Time: " << time_data.back().count() / 1000 << "μs"
-            << " BW (Dev to Host): "
-            << ((double)sizeBytes * 1024) / time_data.back().count() << " MB/s"
+            << " BW (each direction): "
+            << ((double)sizeBytes * 1000) / time_data.back().count() << " MB/s"
             << std::endl;
 
   cudaFreeHost(out_host);
@@ -91,16 +103,22 @@ void runPciePinnedWriteCombined(
     cudaStream_t stream_in, cudaStream_t stream_out) {
   long sizeBytes = size * sizeof(float);
 
-  float *out_host;
-  CUDA_CHECK(cudaMallocHost((void **)&out_host, sizeBytes,
-                            cudaHostAllocWriteCombined));
-  float *out_dev;
+  float *in_host, *out_host;
+  CUDA_CHECK(
+      cudaHostAlloc((void **)&out_host, sizeBytes, cudaHostAllocWriteCombined));
+  CUDA_CHECK(
+      cudaHostAlloc((void **)&in_host, sizeBytes, cudaHostAllocWriteCombined));
+  float *in_dev, *out_dev;
+  CUDA_CHECK(cudaMalloc((void **)&in_dev, sizeBytes));
   CUDA_CHECK(cudaMalloc((void **)&out_dev, sizeBytes));
 
   auto ts = std::chrono::system_clock::now();
 
-  CUDA_CHECK(cudaMemcpyAsync((void **)out_host, out_dev, sizeBytes,
-                             cudaMemcpyDeviceToHost, stream_out));
+  CUDA_CHECK(cudaMemcpyAsync((void **)in_host, out_dev, sizeBytes,
+                             cudaMemcpyDeviceToHost, stream_in));
+  CUDA_CHECK(cudaMemcpyAsync((void **)in_dev, out_host, sizeBytes,
+                             cudaMemcpyHostToDevice, stream_out));
+  CUDA_CHECK(cudaStreamSynchronize(stream_in));
   CUDA_CHECK(cudaStreamSynchronize(stream_out));
 
   time_data.push_back(std::chrono::system_clock::now() - ts);
@@ -115,7 +133,7 @@ void runPciePinnedWriteCombined(
 }
 
 void storeTimingsToFile(const std::vector<std::chrono::nanoseconds> &time_data,
-                        const std::vector<int> sizes,
+                        const std::vector<size_t> sizes,
                         const std::string filename) {
   std::ofstream file;
   file.open(filename);
@@ -127,15 +145,19 @@ void storeTimingsToFile(const std::vector<std::chrono::nanoseconds> &time_data,
 }
 
 int main() {
+  int deviceId;
+  cudaGetDevice(&deviceId);
+  cudaDeviceProp props{};
+  cudaGetDeviceProperties(&props, deviceId);
+  std::cout << "Device: " << props.name << "\n";
 
   std::vector<std::chrono::nanoseconds> time_data;
 
-  std::vector<int> SIZES = {1,        2,        4,         8,        16,
-                            32,       64,       128,       256,      512,
-                            1024,     2048,     4096,      8192,     16384,
-                            32768,    65536,    131072,    262144,   524288,
-                            1048576,  2097152,  4194304,   8388608,  16777216,
-                            33554432, 67108864, 134217728, 268435456};
+  std::vector<size_t> SIZES = {
+      8,        16,       32,       64,        128,      256,     512,
+      1024,     2048,     4096,     8192,      16384,    32768,   65536,
+      131072,   262144,   524288,   1048576,   2097152,  4194304, 8388608,
+      16777216, 33554432, 67108864, 134217728, 268435456};
 
   cudaStream_t stream_in, stream_out;
   cudaStreamCreate(&stream_in);
@@ -144,25 +166,30 @@ int main() {
   std::cout << "------- Running PCIe Normal ------"
             << "\n\n";
   for (auto size : SIZES) {
-    runPcieNormal(size, time_data, stream_in, stream_out);
+    runPcieDefault(size, time_data, stream_in, stream_out);
   }
-  storeTimingsToFile(time_data, SIZES, "pcie_normal.csv");
+  storeTimingsToFile(time_data, SIZES, "../pcie_normal.csv");
   time_data.clear();
+
+  // sleep
+  std::this_thread::sleep_for(std::chrono::seconds(5));
 
   std::cout << "\n----- Running PCIe Pinned ------"
             << "\n\n";
   for (auto size : SIZES) {
     runPciePinned(size, time_data, stream_in, stream_out);
   }
-  storeTimingsToFile(time_data, SIZES, "pcie_pinned.csv");
+  storeTimingsToFile(time_data, SIZES, "../pcie_pinned.csv");
   time_data.clear();
+
+  std::this_thread::sleep_for(std::chrono::seconds(5));
 
   std::cout << "\n----- Running PCIe Pinned Write Combined ------"
             << "\n\n";
   for (auto size : SIZES) {
     runPciePinnedWriteCombined(size, time_data, stream_in, stream_out);
   }
-  storeTimingsToFile(time_data, SIZES, "pcie_pinned_write_combined.csv");
+  storeTimingsToFile(time_data, SIZES, "../pcie_pinned_write_combined.csv");
   time_data.clear();
 
   cudaStreamDestroy(stream_in);
