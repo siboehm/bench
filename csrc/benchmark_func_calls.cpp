d97f9e49994057be9e827a2effcd9a82f128dfc7
Simon Boehm <simonboehm@mailbox.org>
2023-05-05 17:57:30 +0200

    Benchmark func calls
    * No more fun safe math
    
    * Benchmark func calls
    
    * benchmark func calls

---
+++ csrc/benchmark_func_calls.cpp
@@ -0,0 +1,30 @@
+#include <benchmark/benchmark.h>
+
+float __attribute__((noinline)) someFunctionNoInline(float a) { return a; }
+
+/*
+Considerations:
+- Cannot use int accumulator, bc the compiler will vectorize (associativity is
+given)
+- Accumulate happens in reg, writes to memory at the end
+*/
+
+float a = 10;
+float result;
+
+static void BM_someFunction(benchmark::State &state) {
+  for (auto _ : state) {
+    result += a;
+  }
+}
+
+static void BM_someFunctionNoInline(benchmark::State &state) {
+  for (auto _ : state) {
+    result += someFunctionNoInline(a);
+  }
+}
+
+BENCHMARK(BM_someFunction);
+BENCHMARK(BM_someFunctionNoInline);
+
+BENCHMARK_MAIN();
