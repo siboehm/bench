29d1fe75122989fb4a7646dbda2f4dabeb60546f
Simon Boehm <mail@siboehm.com>
2023-05-21 22:24:21 +0000

    .

--- csrc/benchmark_fma.cpp
+++ csrc/benchmark_fma.cpp
@@ -1,16 +1,14 @@
-#include <cmath>
 #include <random>
 
 #include "benchmark/benchmark.h"
 
 __attribute__((noinline)) float_t no_fma(float x) {
+  // explicit intermediate precision, hence no FMA emitted w/o fastmath
   float_t y = x * x;
   return 1.0f + y;
 }
 
-__attribute__((noinline)) float_t with_fma(float x) {
-  return std::fma(x, x, 1.0f);
-}
+__attribute__((noinline)) float_t with_fma(float x) { return x * x + 1.0f; }
 
 float val = 0.1;
 
